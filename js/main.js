52f7fb75c6cc8e8f8f16698af7cae29631378314
Md. Saiful Islam <80346558+Coder-Saiful@users.noreply.github.com>
2021-03-12 20:33:14 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,273 @@
+$(document).ready(function () {
+
+    // team slider js
+
+    $('.team-slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+    },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+    },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+    });
+
+    // testimonial slider js
+    $('.testimonial-slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+    }
+            
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+    });
+
+    // sticky top js
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $(".sticky-top").css("border-bottom", "1px solid #ddd");
+        } else {
+            $(".sticky-top").css("border", "0");
+        }
+    });
+
+    // Smooths scrolling
+
+    $('body').scrollspy({
+        target: ".navbar",
+        offset: 50
+    });
+
+
+    //animation scroll js
+    var html_body = $('html, body');
+    $('nav a').on('click', function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                html_body.animate({
+                    scrollTop: target.offset().top - 30
+                }, 1500);
+                return false;
+            }
+        }
+    });
+
+    // wow js
+
+    new WOW().init();
+
+    // particle js
+
+    particlesJS("particles-js", {
+        "particles": {
+            "number": {
+                "value": 100,
+                "density": {
+                    "enable": true,
+                    "value_area": 400
+                }
+            },
+            "color": {
+                "value": "#ffffff"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                },
+                "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                }
+            },
+            "opacity": {
+                "value": 0.5,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 4,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 3,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 170,
+                "color": "#ffffff",
+                "opacity": 0.4,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 3,
+                "direction": "none",
+                "random": true,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "grab"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 300,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 400,
+                    "size": 40,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 100,
+                    "duration": 0.4
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true
+    });
+
+    // scroll to top js
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $(".scroll_to_top").fadeIn();
+        } else {
+            $(".scroll_to_top").fadeOut();
+        }
+    });
+
+    $(".scroll_to_top").click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+    // preloader js
+    
+    $(window).on("load", function(){
+        $(".preloader").delay(3000).fadeOut(700);
+    });
+    
+    // responsive menu js
+    
+    $(".openNav").click(function(){
+        $(".overlay").css({
+            top : "0",
+            transition: "0.5s"
+        });
+    });
+    
+    $(".closeNav").click(function(){
+        $(".overlay").css({
+            top: "-100%",
+            transition: "0.5s"
+        });
+    });
+    
+    $(".overlay_content a").click(function(){
+        $(".overlay").css({
+            top: "-100%",
+            transition: "0.3s"
+        });
+    });
+    
+    $(".overlay_top a img").click(function(){
+        $(".overlay").css({
+            top: "-100%",
+            transition: "0.3s"
+        });
+    });
+
+});
+
+
+
+
+
+
+
+
